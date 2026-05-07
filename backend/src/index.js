727d5a64fa9d11cba1a12aabddcf9202b9bbbba6
mati12012 <matias.caamano2301@alumnos.ubiobio.cl>
2026-05-06 22:03:38 -0400

    para arreglar error al conectar a la BD

--- backend/src/index.js
+++ backend/src/index.js
@@ -21,7 +21,7 @@ connectDb()
     const PORT = process.env.PORT || 3000;
     const HOST = process.env.HOST || "localhost";
 
-    app.listen(PORT, HOST,  () => {
+    app.listen(PORT,  () => {
       console.log(`Servidor iniciado en http://${HOST}:${PORT}`);
     });
   })
